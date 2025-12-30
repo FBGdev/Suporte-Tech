333ebd492644e811f241c4b4015756c0ed76cb2a
FBGdev <fabianodev@yahoo.com>
2025-12-30 19:16:44 -0300

    Update ASGI configuration for suporte project

--- suporte/suporte/asgi.py
+++ suporte/suporte/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suporte.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suporte.suporte.settings')
 
 application = get_asgi_application()
+
