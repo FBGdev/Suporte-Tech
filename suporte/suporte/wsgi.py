abd5e2a76085e6ac2bfacfce9ad0ff8d51f6134b
FBGdev <fabianodev@yahoo.com>
2025-12-30 19:24:42 -0300

    Update wsgi.py

--- suporte/suporte/wsgi.py
+++ suporte/suporte/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suporte.suporte.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suporte.settings')
 
 application = get_wsgi_application()
+
 
