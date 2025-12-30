c698487db04750534735c2ee5a7f32176177e60c
FBGdev <fabianodev@yahoo.com>
2025-12-30 19:29:27 -0300

    Update settings.py

--- suporte/suporte/settings.py
+++ suporte/suporte/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/login/'
 LOGOUT_REDIRECT_URL = '/login/'
+
 
 
